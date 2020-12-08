10516891dcc5f1be8a769751078f40ae8bbae25a
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-08 12:02:37 +0530

    Adding monthly Employee wage UC5

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,25 +2,29 @@
 
 echo "Welcome to the Employee wage computation"
 
-#UC3
+#UC5
 IS_PRESENT=1
 IS_ABSENT=0
 IS_PARTTIME=2
 
 empRatePerHr=20
+numWorkingDays=20
+totalSalary=0
 
-attendanceCheck=$((RANDOM%3))
-
-case "$attendanceCheck" in
-	$IS_PRESENT)
-		empHrs=8
-		salary=$(($empHrs*$empRatePerHr))
-		;;
-	$IS_PARTTIME)
-		empHrs=4
-		salary=$(($empHrs*$empRatePerHr))
-		;;
-	*)
-		salary=0
-		;;
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
+	attendanceCheck=$((RANDOM%3))
+	case "$attendanceCheck" in
+		$IS_PRESENT)
+			empHrs=8
+			;;
+		$IS_PARTTIME)
+			empHrs=4
+			;;
+		*)
+			empHrs=0
+			;;
 esac
+	dailyWage=$(($empHrs*$empRatePerHr))
+	totalSalary=$(($dailyWage+$totalSalary))
+done
