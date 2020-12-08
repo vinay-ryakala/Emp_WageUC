dfd82c585070d29546b04307d45352c38f7324db
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-08 11:26:50 +0530

    Adding Part time Employee and daily wage calculation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,18 +2,23 @@
 
 echo "Welcome to the Employee wage computation"
 
-#UC2
+#UC3
 IS_PRESENT=1
 IS_ABSENT=0
+IS_PARTTIME=2
 
 empRatePerHr=20
-empHrs=8
 
-attendanceCheck=$((RANDOM%2))
+attendanceCheck=$((RANDOM%3))
 
 if [ $attendanceCheck -eq 1 ]
 then
+	empHrs=8
+	salary=$(($empHrs*$empRatePerHr))
+elif [ $attendanceCheck -eq 2 ]
+then
+	empHrs=4
 	salary=$(($empHrs*$empRatePerHr))
 else
-	salary=0
+salary=0
 fi
