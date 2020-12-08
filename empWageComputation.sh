c31bc8f4035d33311e9c3d6725381d1fe94eda3d
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-08 12:52:28 +0530

    Refactoring code to calculate wages till condition is reached UC6

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,29 +2,34 @@
 
 echo "Welcome to the Employee wage computation"
 
-#UC5
+#UC6
 IS_PRESENT=1
 IS_ABSENT=0
 IS_PARTTIME=2
-
 empRatePerHr=20
-numWorkingDays=20
-totalSalary=0
+NumWorkingDays=20
+MaxHrsInMonth=100
 
-for (( day=1; day<=$numWorkingDays; day++ ))
+totalEmpHrs=0
+totalWorkingDays=0
+
+while [[ $totalEmpHrs -lt $MaxHrsInMonth && $totalWorkingDays -lt $NumWorkingDays ]]
 do
-	attendanceCheck=$((RANDOM%3))
-	case "$attendanceCheck" in
-		$IS_PRESENT)
-			empHrs=8
-			;;
-		$IS_PARTTIME)
-			empHrs=4
-			;;
-		*)
-			empHrs=0
-			;;
-esac
-	dailyWage=$(($empHrs*$empRatePerHr))
-	totalSalary=$(($dailyWage+$totalSalary))
+        ((totalWorkingDays++))
+        attendanceCheck=$((RANDOM%3))
+        case "$attendanceCheck" in
+                $IS_PRESENT)
+                        empHrs=8
+                        ;;
+                $IS_PARTTIME)
+                        empHrs=4
+                        ;;
+                *)
+                        empHrs=0
+                        ;;
+        esac
+totalEmpHrs=$(($totalEmpHrs+$empHrs))
 done
+
+totalsalary=$(($totalEmpHrs*$empRatePerHr))
+
