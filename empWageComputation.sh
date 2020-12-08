e410e6d16250a5de4ec983b781027f1b541dba63
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-08 10:35:04 +0530

    Adding Employee attendace check

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,3 +2,15 @@
 
 echo "Welcome to the Employee wage computation"
 
+#UC1
+IS_PRESENT=1
+IS_ABSENT=0
+
+attendanceCheck=$((RANDOM%2))
+
+if [ $attendanceCheck -eq 1 ]
+then
+	echo "Employee is present"
+else
+	echo "Employee is absent"
+fi
